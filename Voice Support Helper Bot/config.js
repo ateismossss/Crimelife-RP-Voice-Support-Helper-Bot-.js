f19c267f70c0e4c7bd5739a43d58158bfce9c8e2
Ateismos <kilianflawless@gmail.com>
2025-08-27 17:26:53 +0200

    Delete Voice Support Helper Bot directory

--- Voice Support Helper Bot/config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    token: 'TOKEN', //Bot Token
-    clientId: '123456789123456789', 
-    guildId: '123456789123456789', 
-    logChannelId: '123456789123456789',                                                                                                                                                                                                                                                                                                                                                            // Coded and skidded  by @ateismos :)
-    voiceChannelId: '123456789123456789' 
-};
