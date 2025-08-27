9e8c5d126f72ec0e8106dc530c47b5473eeeac09
Ateismos <kilianflawless@gmail.com>
2025-08-27 17:23:25 +0200

    Add files via upload

---
+++ Voice Support Helper Bot/README.txt
@@ -0,0 +1,19 @@
+Voice Support Bot
+
+Simple Voice Support Bot to play music or music/info in your waiting room
+
+
+
+IMPORTANT!
+
+You can add your mp3 under /assets (Important!: the file must be named audio.mp3!)
+
+
+Install the following packages in your CMD console!
+
+
+npm install discord.js @discordjs/voice ffmpeg-static
+
+
+
+Coded and skidded  by @ateismos :)
---
+++ Voice Support Helper Bot/config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    token: 'TOKEN', //Bot Token
+    clientId: '123456789123456789', 
+    guildId: '123456789123456789', 
+    logChannelId: '123456789123456789',                                                                                                                                                                                                                                                                                                                                                            // Coded and skidded  by @ateismos :)
+    voiceChannelId: '123456789123456789' 
+};
